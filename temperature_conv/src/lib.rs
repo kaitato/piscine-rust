e159975d7a887df8f42c78f5faab6ec935b40ecc
Karyun Cheung <karyun.cheung@gmail.com>
2023-03-13 12:39:36 +0000

    Fix syntax

--- temperature_conv/src/lib.rs
+++ temperature_conv/src/lib.rs
@@ -1,10 +1,10 @@
 
 pub fn fahrenheit_to_celsius(f: f64) -> f64 {
-    (f - 32.0) * (9.0/5.0);
+    (f - 32.0) * (9.0/5.0)
 }
 
 pub fn celsius_to_fahrenheit(c: f64) -> f64 {
-    (c * (9.0/5.0)) + 32.0;
+    (c * (9.0/5.0)) + 32.0
 }
 
 #[cfg(test)]
@@ -14,8 +14,8 @@ mod tests {
     #[test]
     fn it_works() {
         let celsius = fahrenheit_to_celsius(-459.67);
-        assert_eq!(celsius, -273.15)
+        assert_eq!(celsius, -273.15);
         let fahrenheit = celsius_to_fahrenheit(0.0);
-        assert_eq!(fahrenheit, 32.0)
+        assert_eq!(fahrenheit, 32.0);
     }
 }
