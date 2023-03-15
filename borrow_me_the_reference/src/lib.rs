89f07a7cdfbd879b2cd9b034d9ebfc1e795f684a
karyun.cheung <karyun.cheung@gmail.com>
2023-03-15 14:07:53 +0000

    Do operations

--- borrow_me_the_reference/src/lib.rs
+++ borrow_me_the_reference/src/lib.rs
@@ -1,5 +1,5 @@
 pub fn delete_and_backspace(s: &mut String) {
-    let mut result_string = String::new();
+    // let mut result_string = String::new();
     let mut chars: Vec<char> = Vec::new();
     let mut skip_next_char = false;
 
@@ -22,13 +22,25 @@ pub fn delete_and_backspace(s: &mut String) {
         }
     }
     for c in chars {
-        result_string.push(c);
+        s.push(c);
     }
-    println!("{}",result_string);
 }
 
-// pub fn do_operations(v: &mut Vec<String>) {
-// }
+pub fn do_operations(v: &mut Vec<String>) {
+    for eq in v.iter_mut() {
+        if let Some(op_pos) = eq.find(|c: char| c == '+' || c == '-') {
+            let (left, right) = eq.split_at(op_pos);
+            let left_val = left.trim().parse::<i32>().unwrap();
+            let right_val = right[1..].trim().parse::<i32>().unwrap();
+            let result = match right.chars().next().unwrap() {
+                '+' => left_val + right_val,
+                '-' => left_val - right_val,
+                _ => panic!("Invalid operator found"),
+            };
+            *eq = result.to_string();
+        }
+    }
+}
 
 #[cfg(test)]
 mod tests {
@@ -37,11 +49,11 @@ mod tests {
     #[test]
     fn it_works() {
         let mut a = String::from("bpp--o+er+++sskroi-++lcw");
-        // let mut b: Vec<String> = vec!["2+2", "3+2", "10-3", "5+5"];
+        let mut b: Vec<String> = vec!["2+2", "3+2", "10-3", "5+5"];
     
         delete_and_backspace(&mut a);
-        // do_operations(&mut b);
+        do_operations(&mut b);
     
-        // println!("{:?}", (a, b));
+        println!("{:?}", (a, b));
     }
 }
