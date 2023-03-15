b272eb0407d5556a54620a4a2241e837594827a8
karyun.cheung <karyun.cheung@gmail.com>
2023-03-15 21:27:54 +0000

    Changes

--- arrange_it/src/lib.rs
+++ arrange_it/src/lib.rs
@@ -1,14 +1,10 @@
 pub fn arrange_phrase(phrase: &str) -> String {
     let mut v:Vec<&str> = phrase.split(" ").collect();
     v.sort_by_key(|word| {
-        word.chars()
-            .find(|c| c.is_digit(10))
-            .map(|c| c.to_digit(10).unwrap())
+        word.chars().find(|c| c.is_digit(10)).map(|c| c.to_digit(10).unwrap())
     });
-    let v_without_digits: Vec<String> = v.into_iter()
-                                          .map(|word| word.chars().filter(|c| !c.is_digit(10)).collect())
-                                          .collect();
-    v_without_digits.join(" ")
+    let v2: Vec<String> = v.into_iter().map(|word| word.chars().filter(|c| !c.is_digit(10)).collect()).collect();
+    v2.join(" ")
 }
 
 #[cfg(test)]
