447f76f938c4db81be332538c41eddff27fa85fe
karyun.cheung <karyun.cheung@gmail.com>
2023-03-17 18:13:21 +0000

    lols

---
+++ edit_distance/src/lib.rs
@@ -0,0 +1,38 @@
+pub fn edit_distance(source: &str, target: &str) -> usize {
+    let mut distance = 0;
+    let mut working_target = target.to_string();
+    for char in source.chars() {
+        if let Some(index) = working_target.find(char) {
+            working_target = working_target[..index].to_string() + &working_target[index+1..];
+            // working_target = new_target.to_string();
+        }  else {
+            distance += 1;
+        }
+        println!("{}, {}", working_target, distance);
+        if working_target.len() == 0 {
+            break;
+        }
+    }
+    // if source.len() > target.len() {
+    //     return source.len().abs_diff(target.len())
+    // }
+    distance += source.len().abs_diff(target.len());
+    distance
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+	let source = "alignment";
+	let target = "assignment";
+	println!(
+		"It's necessary to make {} change(s) to {}, to get {}",
+		edit_distance(source, target),
+		source,
+		target
+	);
+    }
+}
