85914c4bfab862f97cb58bc0ff4310faed1ea33b
karyun.cheung <karyun.cheung@gmail.com>
2023-03-17 19:15:42 +0000

    idk

--- edit_distance/src/lib.rs
+++ edit_distance/src/lib.rs
@@ -1,26 +1,36 @@
 pub fn edit_distance(source: &str, target: &str) -> usize {
-    println!("{}, {}", source, target);
+    let source_chars: Vec<char> = source.chars().collect();
+    let target_chars: Vec<char> = target.chars().collect();
+    let source_len = source_chars.len();
+    let target_len = target_chars.len();
 
-    let mut distance = 0;
-    let mut working_target = target.to_string();
-    for char in source.chars() {
-        if let Some(index) = working_target.find(char) {
-            working_target = working_target[..index].to_string() + &working_target[index+1..];
-            // working_target = new_target.to_string();
-        }  else {
-            distance += 1;
-        }
-        println!("{}, {}", working_target, distance);
-        if working_target.len() == 0 {
-            break;
+    // Initialize the distance matrix with 0s
+    let mut dist = vec![vec![0; target_len + 1]; source_len + 1];
+
+    // Fill the first row and column with the distance from the empty string
+    for i in 0..=source_len {
+        dist[i][0] = i;
+    }
+    for j in 0..=target_len {
+        dist[0][j] = j;
+    }
+
+    // Fill the rest of the matrix
+    for i in 1..=source_len {
+        for j in 1..=target_len {
+            let mut cost = 1;
+            if source_chars[i - 1] == target_chars[j - 1] {
+                cost = 0;
+            }
+            dist[i][j] = usize::min(
+                usize::min(dist[i - 1][j] + 1, dist[i][j - 1] + 1),
+                dist[i - 1][j - 1] + cost,
+            );
         }
     }
-//     let source_len = source.len();
-//     if source.len() > target.len() {
-         working_target.len()
-//     } 
-//     distance += source_len.abs_diff(target.len()) ;
-//     distance
+
+    // Return the minimum edit distance
+    dist[source_len][target_len]
 }
 
 #[cfg(test)]
@@ -47,6 +57,14 @@ mod tests {
 	);
     let source = "kitten";
 	let target = "sitting";
+	println!(
+		"It's necessary to make {} change(s) to {}, to get {}",
+		edit_distance(source, target),
+		source,
+		target
+	);
+    let source = "rosettacode";
+	let target = "raisethysword";
 	println!(
 		"It's necessary to make {} change(s) to {}, to get {}",
 		edit_distance(source, target),
