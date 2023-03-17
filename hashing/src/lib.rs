4f92c7ed54fa93c56fc8173509cbd51218f453a2
karyun.cheung <karyun.cheung@gmail.com>
2023-03-17 20:28:27 +0000

    hashing

---
+++ hashing/src/lib.rs
@@ -0,0 +1,49 @@
+use std::collections::HashMap;
+
+pub fn mean(list: &Vec<i32>) -> f64 {
+    let sum: i32 = list.iter().sum();
+    sum as f64 / list.len() as f64
+}
+
+pub fn median(list: &Vec<i32>) -> i32 {
+        let len = list.len();
+    let mid = len / 2;
+    let sorted = &mut list.clone();
+    sorted.sort();
+    if len % 2 == 0 {
+        (sorted[mid - 1] + sorted[mid]) / 2.0 as i32
+    } else {
+        sorted[mid]
+    }
+}
+
+pub fn mode(list: &Vec<i32>) -> i32 {
+    let mut count_map = HashMap::new();
+    for &val in list {
+        let count = count_map.entry(val).or_insert(0);
+        *count += 1;
+    }
+    let mut mode = list[0];
+    let mut max_count = 0;
+    for (&val, &count) in &count_map {
+        if count > max_count {
+            mode = val;
+            max_count = count;
+        }
+    }
+    mode
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+	println!("Hello, world!");
+	let v = vec![4, 7, 5, 2, 5, 1, 3];
+	println!("mean {}", hashing::mean(&v));
+	println!("median {}", hashing::median(&v));
+	println!("mode {}", hashing::mode(&v));
+    }
+}
