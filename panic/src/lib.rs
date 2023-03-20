9280b67d5f7e7902895355d679612680637c7378
karyun.cheung <karyun.cheung@gmail.com>
2023-03-20 16:49:46 +0000

    Panic

--- panic/src/lib.rs
+++ panic/src/lib.rs
@@ -2,26 +2,26 @@ use std::fs::File;
 // use panic::*;
 
 pub fn open_file(s: &str) -> File {
-    let file = File::open(s).unwrap();
+    let file = File::open(s).expect("File not found");
     file
 }
 
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+// #[cfg(test)]
+// mod tests {
+//     use super::*;
 
-    #[test]
-    fn it_works() {
-    let filename = "created.txt";
-    File::create(filename).unwrap();
+//     #[test]
+//     fn it_works() {
+//     let filename = "created.txt";
+//     File::create(filename).unwrap();
 
-    let a = open_file(filename);
-    println!("{:?}", a);
+//     let a = open_file(filename);
+//     println!("{:?}", a);
     
-    fs::remove_file(filename).unwrap();
+//     fs::remove_file(filename).unwrap();
 
-    //It must panic
-    let b = open_file(filename);
-    }
-}
+//     //It must panic
+//     let b = open_file(filename);
+//     }
+// }
