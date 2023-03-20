843903a90deec7bfa144474fd5ff56f01a48daa0
karyun.cheung <karyun.cheung@gmail.com>
2023-03-20 15:53:03 +0000

    unwrap

--- unwrap_or_expect/src/lib.rs
+++ unwrap_or_expect/src/lib.rs
@@ -12,8 +12,8 @@ pub fn fetch_data(server: Result<String, String>, security_level: Security) -> S
         Err(_) => match security_level {
             Security::Unknown => server.unwrap(),
             Security::High => server.expect("ERROR: program stops"),
-            Security::Medium => server.unwrap_or("default_value".to_string()),
-            Security::Low => server.unwrap_or_else(|error| return error),
+            Security::Medium => server.unwrap_or("WARNING: check the server".to_string()),
+            Security::Low => server.unwrap_or_else(|error| return "Not found:".to_string()+ &error),
             Security::BlockServer => server.unwrap_err(),
         }
     }
