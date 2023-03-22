987dbcd9808e2db0b60ccefa64d75a6f125a0eed
karyun.cheung <karyun.cheung@gmail.com>
2023-03-22 16:33:14 +0000

    ROUND

--- macro_calculator/src/lib.rs
+++ macro_calculator/src/lib.rs
@@ -11,12 +11,12 @@ pub struct Food {
 pub fn calculate_macros(foods: Vec<Food>) -> json::JsonValue {
     let calories = foods.iter()
         .fold((0.0,0.0,0.0,0.0),| mut acc, food: &Food| {
-            acc.0 += (((food.calories[1]
+            acc.0 += ((food.calories[1]
                 .replace("kcal", "")
-                .parse::<f64>().unwrap() * food.nbr_of_portions) * 100.) as i64) as f64 / 100.;
-            acc.1 += (((food.carbs * food.nbr_of_portions) * 100.) as i64) as f64 / 100.;
-            acc.2 += (((food.proteins * food.nbr_of_portions) * 100.) as i64) as f64 / 100.;
-            acc.3 += (((food.fats *food.nbr_of_portions) * 100.) as i64) as f64 / 100.;
+                .parse::<f64>().unwrap() * food.nbr_of_portions) * 100.).round() / 100.;
+            acc.1 += ((food.carbs * food.nbr_of_portions) * 100.).round()/ 100.;
+            acc.2 += ((food.proteins * food.nbr_of_portions) * 100.).round() / 100.;
+            acc.3 += ((food.fats *food.nbr_of_portions) * 100.).round() / 100.;
             println!("{:?}", acc);
             acc
             });
