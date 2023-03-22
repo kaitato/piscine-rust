c0130d4553ae15a995acfc3a8ff0d06175134870
karyun.cheung <karyun.cheung@gmail.com>
2023-03-22 16:50:08 +0000

    round function

--- macro_calculator/src/lib.rs
+++ macro_calculator/src/lib.rs
@@ -7,24 +7,28 @@ pub struct Food {
     pub carbs: f64,
     pub nbr_of_portions: f64,
 }
+fn round_to_decimal(num: f64, decimal_places: i32) -> f64 {
+    let multiplier = 10_f64.powi(decimal_places);
+    (num * multiplier).round() / multiplier
+}
 
 pub fn calculate_macros(foods: Vec<Food>) -> json::JsonValue {
     let calories = foods.iter()
         .fold((0.0,0.0,0.0,0.0),| mut acc, food: &Food| {
-            acc.0 += ((format!("{:.2}",(food.calories[1]
+            acc.0 += food.calories[1]
                 .replace("kcal", "")
-                .parse::<f64>().unwrap() * food.nbr_of_portions))).to_string()).parse::<f64>().unwrap();
-            acc.1 += format!("{:.2}",(food.carbs *food.nbr_of_portions).to_string()).parse::<f64>().unwrap();
-            acc.2 += format!("{:.2}",(food.proteins *food.nbr_of_portions).to_string()).parse::<f64>().unwrap();
-            acc.3 += format!("{:.2}",(food.fats *food.nbr_of_portions).to_string()).parse::<f64>().unwrap();
+                .parse::<f64>().unwrap() * food.nbr_of_portions;
+            acc.1 += food.carbs * food.nbr_of_portions;
+            acc.2 += food.proteins * food.nbr_of_portions;
+            acc.3 += food.fats *food.nbr_of_portions;
             println!("{:?}", acc);
             acc
             });
     let result = object!{
-        cals: calories.0,
-       carbs: calories.1,
-        proteins: calories.2 ,
-        fats: calories.3,
+        cals: round_to_decimal(calories.0, 2),
+       carbs: round_to_decimal(calories.1, 2),
+        proteins: round_to_decimal(calories.2, 2),
+        fats: round_to_decimal(calories.3, 2),
     };
     result
     
