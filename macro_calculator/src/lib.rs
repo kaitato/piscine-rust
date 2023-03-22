291f5c69500f0652d0bfd5b3c414c7671e567b05
karyun.cheung <karyun.cheung@gmail.com>
2023-03-22 16:16:56 +0000

    round

--- macro_calculator/src/lib.rs
+++ macro_calculator/src/lib.rs
@@ -11,20 +11,20 @@ pub struct Food {
 pub fn calculate_macros(foods: Vec<Food>) -> json::JsonValue {
     let calories = foods.iter()
         .fold((0.0,0.0,0.0,0.0),| mut acc, food: &Food| {
-            acc.0 += (((food.calories[1]
+            acc.0 += ((food.calories[1]
                 .replace("kcal", "")
-                .parse::<f64>().unwrap() * food.nbr_of_portions) * 100.).round())/100.;
-            acc.1 += (((food.carbs * food.nbr_of_portions) * 100.).round())/100. ;
-            acc.2 += (((food.proteins * food.nbr_of_portions) * 100.).round())/100.;
-            acc.3 += (((food.fats *food.nbr_of_portions) * 100.).round())/100.;
+                .parse::<f64>().unwrap() * food.nbr_of_portions) as i64 * 100) as f64 / 100.;
+            acc.1 += ((food.carbs * food.nbr_of_portions) as i64 * 100) as f64 / 100.;
+            acc.2 += ((food.proteins * food.nbr_of_portions) as i64 * 100) as f64 / 100.;
+            acc.3 += ((food.fats *food.nbr_of_portions) as i64 * 100) as f64 / 100.;
             println!("{:?}", acc);
             acc
             });
     let result = object!{
         calories: calories.0,
        carbs: calories.1,
-        proteins: calories.2 * 100.0,
-        fats: calories.3 * 100.0,
+        proteins: calories.2 ,
+        fats: calories.3,
     };
     result
     
