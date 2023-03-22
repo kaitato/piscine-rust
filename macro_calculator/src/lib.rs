77a2b8c871e21fcb37981aa08eea74b5341c0367
karyun.cheung <karyun.cheung@gmail.com>
2023-03-22 16:19:46 +0000

    check 0 case

--- macro_calculator/src/lib.rs
+++ macro_calculator/src/lib.rs
@@ -9,6 +9,15 @@ pub struct Food {
 }
 
 pub fn calculate_macros(foods: Vec<Food>) -> json::JsonValue {
+    if foods.len() == 0 {
+        let result = object!{
+            calories: 0,
+           carbs: 0,
+            proteins: 0,
+            fats: 0,
+        };
+        return result
+    }
     let calories = foods.iter()
         .fold((0.0,0.0,0.0,0.0),| mut acc, food: &Food| {
             acc.0 += ((food.calories[1]
