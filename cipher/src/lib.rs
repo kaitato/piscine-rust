3b8f2209ca3791b574e0f76668b1cba8cf1175ee
karyun.cheung <karyun.cheung@gmail.com>
2023-03-22 00:37:41 +0000

    Cipher

---
+++ cipher/src/lib.rs
@@ -0,0 +1,44 @@
+#[derive(Debug, Clone, Eq, PartialEq)]
+pub struct CipherError {
+    validation: bool,
+    expected: String,
+}
+
+impl CipherError {
+    pub fn new(validation: bool, expected: String) -> CipherError {
+        CipherError { validation, expected }
+    }
+}
+
+pub fn cipher(original: &str, ciphered: &str) -> Option<Result<bool, CipherError>> {
+    let atbash_cipher = original
+        .chars()
+        .map(|c| {
+            if c.is_ascii_alphabetic() {
+                let diff = if c.is_ascii_uppercase() { b'A' } else { b'a' };
+                (b'z' - (c as u8 - diff)) as char
+            } else {
+                c
+            }
+        })
+        .collect::<String>();
+    if atbash_cipher == ciphered {
+        Some(Ok(true))
+    } else {
+        let expected_cipher = atbash_cipher.clone();
+        let err = CipherError::new(false, expected_cipher);
+        Some(Err(err))
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+    println!("{:?}", cipher("1Hello 2world!", "1Svool 2dliow!"));
+    println!("{:?}", cipher("1Hello 2world!", "svool"));
+    println!("{:?}", cipher("", "svool"));
+    }
+}
