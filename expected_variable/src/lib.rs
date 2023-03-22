72d57247550c2f99bf5e388e5d0cb57c99187ee0
karyun.cheung <karyun.cheung@gmail.com>
2023-03-22 21:42:35 +0000

    idk

--- expected_variable/src/lib.rs
+++ expected_variable/src/lib.rs
@@ -8,24 +8,23 @@ pub use edit_distance::edit_distance;
 
 
 pub fn expected_variable(string_to_compare: &str, expected_string: &str) -> Option<String> {
-    if string_to_compare.is_camel_lowercase() == false && string_to_compare.contains('_') == false{
+    if string_to_compare.is_camel_lowercase() == false && string_to_compare.contains('_') == false {
         return None
     } else {
         let distance  = edit_distance(&string_to_compare.to_lowercase(), &expected_string.to_lowercase());
-        let mut alikeness = 100;
-        if (distance / string_to_compare.len()) > 100 && (distance / string_to_compare.len()) < 50 {
+        let mut alikeness = 0;
+        if (distance / string_to_compare.len()) > 100{
             alikeness = 0;        
         } else {
             alikeness = 100 - (distance * 100 / expected_string.len());
 
         }
-        println!("{}, {}, {}, {}", alikeness, distance, expected_string.len(), string_to_compare.len());
-        println!("{}", Some(alikeness.to_string() + "%").unwrap());
-        if alikeness > 50 {
-            Some(alikeness.to_string() + "%")
-        } else {
-            return None
-        }
+        // println!("{}, {}, {}, {}", alikeness, distance, expected_string.len(), string_to_compare.len());
+        // println!("{}", Some(alikeness.to_string() + "%").unwrap());
+        match alikeness {
+            a if a > 50 => Some(alikeness.to_string() + "%"),
+            _ => None,
+            }
     }
 }
 
