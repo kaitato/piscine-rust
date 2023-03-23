e10ef5c2a4acb461cf663cfb46dd34b6dc18ea3b
karyun.cheung <karyun.cheung@gmail.com>
2023-03-23 13:35:53 +0000

    THIS IS the ordinal?

---
+++ ordinal/src/lib.rs
@@ -0,0 +1,30 @@
+pub fn num_to_ordinal(x: u32) -> String {
+    let last_num = x % 10;
+    let mut res = x.to_string();
+    if x > 10 && x < 14 {
+        res.push_str("th");
+        return res
+    }
+    match last_num {
+        0 | 4 | 5 | 6 | 7 | 8 | 9 => res.push_str("th"),
+        1 => res.push_str("st"),
+        2 => res.push_str("nd"),
+        3 => res.push_str("rd"),
+        _ => return res,
+    };
+    res
+    
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+    println!("{}", num_to_ordinal(1));
+    println!("{}", num_to_ordinal(22));
+    println!("{}", num_to_ordinal(43));
+    println!("{}", num_to_ordinal(47));
+    }
+}
