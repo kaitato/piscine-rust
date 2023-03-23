1860c90e87acdb3b481d2b808e80528be73fe951
karyun.cheung <karyun.cheung@gmail.com>
2023-03-23 15:09:47 +0000

    eh

--- talking/src/lib.rs
+++ talking/src/lib.rs
@@ -2,14 +2,14 @@ pub fn talking(text: &str) -> &str {
     // if text.chars().all(|c| c.is_uppercase()) {
     if text.is_empty() || text.chars().all(|c| c.is_alphanumeric() == false) {
         return "Just say something!"
+    } else if text.chars().last() == Some('?') &&  !text.chars().filter(|c| c.is_alphabetic()).all(|c|c.is_ascii_uppercase()){
+        return "Sure."
     } else if text.chars().filter(|c| c.is_alphabetic()).all(|c|c.is_ascii_uppercase()) && text.chars().any(|c| !c.is_alphanumeric()){
         if text.chars().last() == Some('?') {
             return "Quiet, I am thinking!"
         }else {
             return "There is no need to yell, calm down!"
         }
-    } else if text.chars().last() == Some('?') {
-        return "Sure."
     } else {
         return "Interesting"
     }
