612516eb5614d2fffe6df37e0ecbce6844dac0d5
karyun.cheung <karyun.cheung@gmail.com>
2023-03-23 15:20:51 +0000

    ?

--- talking/src/lib.rs
+++ talking/src/lib.rs
@@ -1,15 +1,28 @@
 pub fn talking(text: &str) -> &str {
     // if text.chars().all(|c| c.is_uppercase()) {
+    // if text.is_empty() || text.chars().all(|c| c.is_alphanumeric() == false) {
+    //     return "Just say something!"
+    // } else if text.chars().last() == Some('?') && text.to_uppercase() != text && text.chars().any(|c| c.is_alphanumeric()){
+    //     return "Sure."
+    // } else if text.to_uppercase() == text {
+    //     if text.chars().last() == Some('?') {
+    //         return "Quiet, I am thinking!"
+    //     }else {
+    //         return "There is no need to yell, calm down!"
+    //     }
+    // } else {
+    //     return "Interesting"
+    // }
     if text.is_empty() || text.chars().all(|c| c.is_alphanumeric() == false) {
         return "Just say something!"
-    } else if text.chars().last() == Some('?') && text.to_uppercase() != text && text.chars().any(|c| c.is_alphanumeric()){
-        return "Sure."
-    } else if text.to_uppercase() == text {
-        if text.chars().last() == Some('?') {
+    } else if text.chars().last() == Some('?') {
+        if text.to_uppercase() == text {
             return "Quiet, I am thinking!"
-        }else {
-            return "There is no need to yell, calm down!"
+        } else {
+            return "Sure."
         }
+    } else if text.to_uppercase() == text {
+        return "There is no need to yell, calm down!"
     } else {
         return "Interesting"
     }
