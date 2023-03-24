73a655cb02011211f120beb5a5089919f8d41a8d
karyun.cheung <karyun.cheung@gmail.com>
2023-03-24 14:27:46 +0000

    igpay atinlay

--- pig_latin/src/lib.rs
+++ pig_latin/src/lib.rs
@@ -1,9 +1,12 @@
 pub fn pig_latin(text: &str) -> String {
     let vowels = "aeiouAEIOU";
     let words: Vec<&str> = text.split(' ').collect();
+
     let mut result = String::new();
+
     for (i, word) in words.iter().enumerate() {
         let mut new_word = String::new();
+
         let mut chars = word.chars();
         if let Some(first_char) = chars.next() {
             if vowels.contains(first_char) {
@@ -13,8 +16,16 @@ pub fn pig_latin(text: &str) -> String {
                 let mut consonants = String::new();
                 consonants.push(first_char);
 
+                if consonants == "q" {
+                    if let Some(next_char) = chars.next() {
+                        if next_char == 'u' {
+                            consonants.push(next_char);
+                        }
+                    }
+                }
+
                 while let Some(char) = chars.next() {
-                    if vowels.contains(char) || (consonants.ends_with("q") && char == 'u') {
+                    if vowels.contains(char) {
                         break;
                     }
                     consonants.push(char);
