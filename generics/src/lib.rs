95adb7417f3fb6a85838b0363c0684b0e3f11642
karyun.cheung <karyun.cheung@gmail.com>
2023-03-29 13:51:17 +0100

    Generic

--- generics/src/lib.rs
+++ generics/src/lib.rs
@@ -1,4 +1,14 @@
-pub fn identity<T>(v: &T) -> &T {
+pub fn identity<T>(v: T) -> T {
     v
 }
 
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        println!("{}", identity("Hello, world!"));
+        println!("{}", identity(3));
+    }
+}
