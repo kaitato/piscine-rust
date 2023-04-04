a74082d03c0abf31eb6d28f6fb745cacb21d1442
karyun.cheung <karyun.cheung@gmail.com>
2023-04-04 14:16:22 +0100

    lifetimes?

---
+++ lifetimes/src/lib.rs
@@ -0,0 +1,23 @@
+#[derive(Debug)]
+pub struct Person<'a>{
+	pub name: &'a str,
+	pub age: u8,
+}
+
+impl Person<'_> {
+	pub fn new(name: &str) -> Person {
+        Person { name, age: 0 }
+	}
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let person = Person::new("Leo");
+
+        println!("Person = {:?}", person);
+    }
+}
