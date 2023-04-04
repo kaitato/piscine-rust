40c666def0443167696b76af5a918293e16698d0
karyun.cheung <karyun.cheung@gmail.com>
2023-04-04 13:56:48 +0100

    Matrix

--- matrix/src/lib.rs
+++ matrix/src/lib.rs
@@ -1,39 +1,117 @@
 use std::ops::{Index, IndexMut};
-use lalgebra_scalar::{Scalar, Field};
 
+
+pub trait Scalar: Sized{
+    type Item;
+    fn zero() -> Self::Item;
+    fn one() -> Self::Item;
+}
+impl Scalar for u32 {
+    type Item = u32;
+    fn zero() -> Self::Item {
+        0
+    }
+    fn one() -> Self::Item {
+        1
+    }
+}
+impl Scalar for u64 {
+    type Item = u64;
+    fn zero() -> Self::Item {
+        0
+    }
+    fn one() -> Self::Item {
+        1
+    }
+}
+impl Scalar for i32 {
+    type Item = i32;
+    fn zero() -> Self::Item {
+        0
+    }
+    fn one() -> Self::Item {
+        1
+    }
+}
+impl Scalar for i64 {
+    type Item = i64;
+    fn zero() -> Self::Item {
+        0
+    }
+    fn one() -> Self::Item {
+        1
+    }
+}
+impl Scalar for f32 {
+    type Item = f32;
+    fn zero() -> Self::Item {
+        0.
+    }
+    fn one() -> Self::Item {
+        1.
+    }
+}
+impl Scalar for f64 {
+    type Item = f64;
+    fn zero() -> Self::Item {
+        0.
+    }
+    fn one() -> Self::Item {
+        1.
+    }
+}
+#[derive(Debug)]
 pub struct Matrix<T>(pub Vec<Vec<T>>);
 
 impl <T: Scalar<Item = T>> Matrix<T> {
 	pub fn new() -> Matrix<T> {
-        Matrix(vec![vec![T::zero(); 1]])
+        Matrix(vec![vec![T::zero()]])
 	}
-
 	pub fn zero(row: usize, col: usize) -> Matrix<T> {
-        Matrix(vec![vec![T::zero(); col]; row])
-	}
-
-	pub fn identity(n: usize) -> Matrix<T> {
-        let mut matrix = Matrix::zero(n, n);
-        for i in 0..n {
-            matrix[i][i] = T::one();
+        let mut new_vec: Vec<Vec<T>> = Vec::new();
+        for _ in 0..col {
+            let mut new_row: Vec<T> = Vec::new();
+            for _ in 0..row {
+                new_row.push(T::zero());
+            }
+            new_vec.push(new_row);
         }
-        matrix
+        Matrix(new_vec)
+	}
+	pub fn identity(n: usize) -> Matrix<T> {
+        let mut new_vec: Vec<Vec<T>> = Vec::new();
+        for i in 0..n {
+            let mut new_row: Vec<T> = Vec::new();
+            for j in 0..n {
+                if i == j {
+                    new_row.push(T::one())
+                } else {
+                    new_row.push(T::zero())
+                }
+            }
+            new_vec.push(new_row)
+        }
+        Matrix(new_vec)
 	}
 }
-
-impl<T> Index<usize> for Matrix<T> {
-    type Output = Vec<T>;
-
-    fn index(&self, i: usize) -> &Vec<T> {
-        &self.0[i]
-    }
-}
-
-impl<T> IndexMut<usize> for Matrix<T> {
-    fn index_mut(&mut self, i: usize) -> &mut Vec<T> {
-        &mut self.0[i]
-    }
-}
+// pub fn identity(n: usize) -> Matrix<T> {
+//     let mut matrix = Matrix::zero(n, n);
+//     for i in 0..n {
+//         matrix[i][i] = T::one();
+//     }
+//     matrix
+// }
+// impl<T> Index<usize> for Matrix<T> {
+//     type Output = Vec<T>;
+//     fn index(&self, i: usize) -> &Vec<T> {
+//         &self.0[i]
+//     }
+// }
+// impl<T> IndexMut<usize> for Matrix<T> {
+//     fn index_mut(&mut self, i: usize) -> &mut Vec<T> {
+//         &mut self.0[i]
+//     }
+// }
 
 #[cfg(test)]
 mod tests {
