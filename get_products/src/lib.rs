6a89df533f1068898be8678b8c021257748cf5f9
karyun.cheung <karyun.cheung@gmail.com>
2023-04-06 13:00:41 +0100

    get products

---
+++ get_products/src/lib.rs
@@ -0,0 +1,29 @@
+pub fn get_products(arr: Vec<usize>) -> Vec<usize> {
+    let mut products = vec![1; arr.len()];
+
+    let mut product_so_far = 1;
+    for i in 0..arr.len() {
+        products[i] *= product_so_far;
+        product_so_far *= arr[i];
+    }
+
+    product_so_far = 1;
+    for i in (0..arr.len()).rev() {
+        products[i] *= product_so_far;
+        product_so_far *= arr[i];
+    }
+
+    products
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let arr: Vec<usize> = vec![1, 7, 3, 4];
+        let output = get_products(arr);
+        println!("{:?}", output);
+    }
+}
